68bb17cae5cb139220713a87343aa67fecb05f3d
Sinan Qarhiely <sinan.qarhiely@gmail.com>
2022-11-12 01:42:56 +0100

    fix some bugs

---
+++ lib/thems.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import '../constans.dart';
+
+IconData _iconDark = Icons.nights_stay_outlined;
+IconData _iconLight = Icons.wb_sunny_outlined;
+
+ThemeData _darkTheme = ThemeData(
+    appBarTheme: AppBarTheme(
+        backgroundColor: KAppBarColor,
+        toolbarTextStyle: TextStyle(
+          color: Colors.white,
+        )));
